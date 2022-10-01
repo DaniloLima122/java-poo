587a8777c894c11e4c813bb8b835cc43e98d2e87
DaniloLima122 <limadanilo06@gmail.com>
2022-10-01 19:32:39 -0300

    refactor(java-poo): it creates funcionario abstraction

---
+++ src/Secretaria.java
@@ -0,0 +1,23 @@
+import abstractClasses.Funcionario;
+
+public class Secretaria extends Funcionario {
+
+    private double previdencia;
+
+    public double getPrevidencia() {
+        return previdencia;
+    }
+
+    public void setPrevidencia(double previdencia) {
+        this.previdencia = previdencia;
+    }
+
+    public Secretaria(int ID, String nome, double salario){
+        super(ID, nome, salario);
+        // Desconto fixo de um sistema de previdência da empresa
+        this.setPrevidencia(0.05);
+    }
+    public double getSalario (){
+        return(salario*(1-this.getPrevidencia()));
+    }
+}
---
+++ src/abstractClasses/Funcionario.java
@@ -0,0 +1,35 @@
+package abstractClasses;
+
+public abstract class Funcionario {
+    protected int ID;
+    protected String nome;
+    protected double salario;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Funcionario(int ID, String nome, double salario) {
+        this.setID(ID);
+        this.setNome(nome);
+        this.setSalario(salario);
+    }
+
+    protected abstract double getSalario();
+}
